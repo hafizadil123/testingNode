dddb27a7f31830fdb636a0d809449e98d31f9372
Adil Sikander <adilsikander@192.168.10.8>
2020-01-08 00:39:35 +0500

    changes

--- app/lib/fetchEmailParser.js
+++ app/lib/fetchEmailParser.js
@@ -129,8 +129,8 @@ export const events = async (req, res) => {
             }
             if (x == eventsDta.length - 1) {
                const emailFetchedData = eventsDta.map((item) => {
-               const userId = findUserIdByEmail(item.Organizer);
-             //  const userId = '5dfb604ef71b907b18a8dda9';
+              // const userId = findUserIdByEmail(item.Organizer);
+              const userId = '5e14c1ff2824c72182d0417e';
                 if (userId) {
                     const mapData = {
                         subject: item.Subject,
