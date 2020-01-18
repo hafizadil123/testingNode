ddaaa71c2de07d831ac4614a3e375b66df503e35
Adil Sikander <adilsikander@ml-mbps-mbp.home>
2020-01-18 19:26:22 +0500

    chagnes

--- app/controllers/meetings.controller.js
+++ app/controllers/meetings.controller.js
@@ -7,7 +7,7 @@ class MeetingsController extends BaseController {
   whitelist = ['text'];
 
   // Middleware to populate post based on url param
-  _populate = async (req, res, next) => {
+  _populate = async(req, res, next) => {
     const { meetingId } = req.query;
     let result;
     try {
@@ -37,14 +37,14 @@ class MeetingsController extends BaseController {
       const matchedResults = mergedResults.map((item) =>
         getAnsMerged.filter((el) => el.id == item)
       );
-      let removeArraySymbol = [].concat.apply([], matchedResults || []);
+      let removeArraySymbol = [].concat.apply([], matchedResults);
       const total =removeArraySymbol.length > 0 && removeArraySymbol.reduce((a, b) => ({
         weightage: a.weightage + b.weightage,
       }, 0)) || 0;
       // count total and devide to total
-      const avgMeetingScore = Math.trunc(
-        total.weightage / removeArraySymbol.length
-      );
+      const avgMeetingScore = removeArraySymbol.length > 0 ? Math.trunc(
+        total / removeArraySymbol.length
+      ) : 0;
       // res.json(mergedResults);
       res.json({
         subject,
@@ -62,7 +62,7 @@ class MeetingsController extends BaseController {
     }
   };
 
-  search = async (_req, res, next) => {
+  search = async(_req, res, next) => {
     try {
       const posts = await Meeting.find({}).populate({
         path: '_user',
@@ -79,7 +79,7 @@ class MeetingsController extends BaseController {
    * req.post is populated by middleware in routes.js
    */
 
-  fetch = async (req, res, _next) => {
+  fetch = async(req, res, _next) => {
     const userId = req.query.userId;
     const userMeetings = await Meeting.find({ _user: userId }).populate(
       '_user'
@@ -88,7 +88,7 @@ class MeetingsController extends BaseController {
 
     //  userMeetings me sari ek user ki meetings mil rahi hain... meny us meeting kitny members r kitny bndo ny feedback
     const getMembersArray = userMeetings.map(
-      async (item) => await this.getMembers(item._id)
+      async(item) => await this.getMembers(item._id)
     );
     const result = Promise.all(getMembersArray);
     result.then((data) => {
@@ -106,7 +106,7 @@ class MeetingsController extends BaseController {
     });
   };
 
-  getMembers = async (id) => {
+  getMembers = async(id) => {
     const meetingId = id;
     // const meetingId = '5dffad06a9d36afc546e5f6d';
     const members = await Meeting.findOne({ _id: meetingId });
@@ -119,13 +119,13 @@ class MeetingsController extends BaseController {
     return membersFeedback;
   };
 
-  getFeedbacks = async () => {};
+  getFeedbacks = async() => {};
 
   /**
    * req.user is populated by middleware in routes.js
    */
 
-  create = async (req, res, next) => {
+  create = async(req, res, next) => {
     const params = this.filterParams(req.body, this.whitelist);
 
     const post = new Meeting({
@@ -140,7 +140,7 @@ class MeetingsController extends BaseController {
     }
   };
 
-  delete = async (req, res, next) => {
+  delete = async(req, res, next) => {
     /**
      * Ensure the user attempting to delete the post owns the post
      *
