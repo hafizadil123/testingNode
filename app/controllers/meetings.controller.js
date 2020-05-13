8db3297e26a40ee234f68d97ce887e12b5805925
waqasahmad34 <48016736+waqasahmad34@users.noreply.github.com>
2020-05-13 13:48:03 +0500

    change in meetings card api

--- app/controllers/meetings.controller.js
+++ app/controllers/meetings.controller.js
@@ -132,7 +132,7 @@ class MeetingsController extends BaseController {
 		const totalMeetings = await Meeting.find({}).count();
 		const totalFeedbacks = await Feedback.find({}).count();
 		const totalInvitees = await Invite.find({}).count();
-		if (!thisWeekMeetings || !totalMeetings || !totalFeedbacks || !totalInvitees) {
+		if (!totalMeetings || !totalFeedbacks || !totalInvitees) {
 			return res.status(400).json({ message: 'not found!' });
 		}
 		const responseObj = {
